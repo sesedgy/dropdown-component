607220723cf61b5ec056c83f6357e8e63404d874
Луканин Михаил <m_lukanin@itgroup.local>
2019-01-31 18:25:38 +0300

    Updates

---
+++ precache-manifest.5508594f1f1ba7d0286a5ec7a48f7ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06f7afdfaec7ff7fcbc6",
+    "url": "/dropdown-component/static/js/runtime~main.06f7afdf.js"
+  },
+  {
+    "revision": "6d5b9ba5eb0d69358a66",
+    "url": "/dropdown-component/static/js/main.6d5b9ba5.chunk.js"
+  },
+  {
+    "revision": "5426e974ad792de9f7ad",
+    "url": "/dropdown-component/static/js/1.5426e974.chunk.js"
+  },
+  {
+    "revision": "6d5b9ba5eb0d69358a66",
+    "url": "/dropdown-component/static/css/main.8fd17a46.chunk.css"
+  },
+  {
+    "revision": "5426e974ad792de9f7ad",
+    "url": "/dropdown-component/static/css/1.a4ecaa0e.chunk.css"
+  },
+  {
+    "revision": "b00ce6787b81a0f3cb519dbe630db4ad",
+    "url": "/dropdown-component/index.html"
+  }
+];
